b45a5ac4526cc343868d75279d4bcb82c39a59de
nemaba <nelsonmb@gmail.com>
2018-12-17 18:36:56 +0100

    Templates context

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -3,8 +3,24 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, "home_page.html", {})
+    context = {
+        "title": "Hello World!",
+        "content": "Welcome home page"
+    }
+    return render(request, "home_page.html", context)
 
-def home_page_old(request):
-    html_ = """"""
-    return HttpResponse(html_)
+
+def about_page(request):
+    context = {
+        "title": "About Page",
+        "content": "Welcome about page"
+    }
+    return render(request, "about_page.html", context)
+
+
+def contact_page(request):
+    context = {
+        "title": "Contact Page",
+        "content": "Welcome contact page"
+    }
+    return render(request, "contact_page.html", context)
