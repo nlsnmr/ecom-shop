4efb6ea88e754da59ae4911a2d0bee3f049f1815
nemaba <nelsonmb@gmail.com>
2018-12-20 12:24:15 +0100

    reverse for urls

--- products/urls.py
+++ products/urls.py
@@ -5,7 +5,9 @@ from .views import (
     ProductDetailSlugView
     )
 
+app_name = 'products'
+
 urlpatterns = [
-    url(r'^$', ProductListView.as_view()),
-    url(r'^(?P<slug>[\w-]+)/$', ProductDetailSlugView.as_view())
+    url(r'^$', ProductListView.as_view(), name='list'),
+    url(r'^(?P<slug>[\w-]+)/$', ProductDetailSlugView.as_view(), name='detail')
 ]
