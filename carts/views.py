eeaf523f8d265deecabeb45750f977df48b2bf90
nemaba <nelsonmb@gmail.com>
2018-12-22 11:13:28 +0100

    cart model

--- carts/views.py
+++ carts/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your models here.
 def cart_home(request):
+    cart_id = request.session.get("cart_id", None)
+    if cart_id is None:
+        request.session['cart_id'] = 12
     return render(request, "carts/home.html", {})
