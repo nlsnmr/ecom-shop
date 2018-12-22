da53024600a58ee474342bc6e900d015b2911c43
nemaba <nelsonmb@gmail.com>
2018-12-22 12:00:46 +0100

    cart model manager

--- carts/views.py
+++ carts/views.py
@@ -9,11 +9,13 @@ def cart_create(user=None):
 
 def cart_home(request):
     cart_id = request.session.get("cart_id", None)
-
-    qs = Cart.objects.get(id=cart_id)
+    qs = Cart.objects.filter(id=cart_id)
     if qs.count() == 1:
         cart_obj = qs.first()
+        if request.user.is_authenticated and cart_obj.user is None:
+            cart_obj.user = request.user
+            cart_obj.save()
     else:
-        cart_obj = cart_create()
+        cart_obj = Cart.objects.new(user=request.user)
         request.session['cart_id'] = cart_obj.id
     return render(request, "carts/home.html", {})
