47c94bde15f43ed54a400402f8af75eadfe83acd
nemaba <nelsonmb@gmail.com>
2018-12-22 17:54:32 +0100

    cart model manager - update

--- carts/models.py
+++ carts/models.py
@@ -4,13 +4,30 @@ from products.models import Product
 
 User = settings.AUTH_USER_MODEL
 
+
 class CartManager(models.Manager):
+    def new_or_get(self, request):
+        cart_id = request.session.get("cart_id", None)
+        qs = self.get_queryset().filter(id=cart_id)
+        if qs.count() == 1:
+            new_obj = False
+            cart_obj = qs.first()
+            if request.user.is_authenticated and cart_obj.user is None:
+                cart_obj.user = request.user
+                cart_obj.save()
+        else:
+            cart_obj = self.new(user=request.user)
+            request.session['cart_id'] = cart_obj.id
+        return cart_obj, new_obj
+
     def new(self, user=None):
+        new_obj = True
         user_obj = None
         if user is not None:
             if user.is_authenticated:
                 user_obj = user
         return self.model.objects.create(user=user_obj)
+
 
 class Cart(models.Model):
     user = models.ForeignKey(User, null=True, blank=True, on_delete=False)
