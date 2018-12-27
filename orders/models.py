5c68414c679114ebd704faa561b30ba13e3c791d
nemaba <nelsonmb@gmail.com>
2018-12-27 19:04:52 +0100

    order manager

--- orders/models.py
+++ orders/models.py
@@ -1,10 +1,12 @@
 import math
-
+import datetime
 from django.db import models
 from django.db.models.signals import pre_save, post_save
 from billing.models import BillingProfile
 from carts.models import Cart
 from ecommerce.utils import unique_order_id_generator
+from django.utils import timezone
+from django.db.models import Count, Sum, Avg
 
 ORDER_STATUS_CHOICES = (
     ('created', 'Created'),
@@ -13,6 +15,94 @@ ORDER_STATUS_CHOICES = (
     ('refunded', 'Refunded'),
     ('canceled', 'Canceled'),
 )
+
+class OrderManagerQuerySet(models.query.QuerySet):
+    def recent(self):
+        return self.order_by("-updated", "-timestamp")
+
+    def get_sales_breakdown(self):
+        recent = self.recent().not_refunded()
+        recent_data = recent.totals_data()
+        recent_cart_data = recent.cart_data()
+        shipped = recent.not_refunded().by_status(status='shipped')
+        shipped_data = shipped.totals_data()
+        paid = recent.by_status(status='paid')
+        paid_data = paid.totals_data()
+        data = {
+            'recent': recent,
+            'recent_data':recent_data,
+            'recent_cart_data': recent_cart_data,
+            'shipped': shipped,
+            'shipped_data': shipped_data,
+            'paid': paid,
+            'paid_data': paid_data
+        }
+        return data
+
+    def by_weeks_range(self, weeks_ago=7, number_of_weeks=2):
+        if number_of_weeks > weeks_ago:
+            number_of_weeks = weeks_ago
+        days_ago_start = weeks_ago * 7  # days_ago_start = 49
+        days_ago_end = days_ago_start - (number_of_weeks * 7) #days_ago_end = 49 - 14 = 35
+        start_date = timezone.now() - datetime.timedelta(days=days_ago_start)
+        end_date = timezone.now() - datetime.timedelta(days=days_ago_end)
+        return self.by_range(start_date, end_date=end_date)
+
+    def by_range(self, start_date, end_date=None):
+        if end_date is None:
+            return self.filter(updated__gte=start_date)
+        return self.filter(updated__gte=start_date).filter(updated__lte=end_date)
+
+    def by_date(self):
+        now = timezone.now() - datetime.timedelta(days=9)
+        return self.filter(updated__day__gte=now.day)
+
+    def totals_data(self):
+        return self.aggregate(Sum("total"), Avg("total"))
+
+    def cart_data(self):
+        return self.aggregate(
+                        Sum("cart__products__price"),
+                        Avg("cart__products__price"),
+                        Count("cart__products")
+                                    )
+
+    def by_status(self, status="shipped"):
+        return self.filter(status=status)
+
+    def not_refunded(self):
+        return self.exclude(status='refunded')
+
+    def by_request(self, request):
+        billing_profile, created = BillingProfile.objects.new_or_get(request)
+        return self.filter(billing_profile=billing_profile)
+
+    def not_created(self):
+        return self.exclude(status='created')
+
+class OrderManager(models.Manager):
+    def get_queryset(self):
+        return OrderManagerQuerySet(self.model, using=self._db)
+
+    def by_request(self, request):
+        return self.get_queryset().by_request(request)
+
+    def new_or_get(self, billing_profile, cart_obj):
+        created = False
+        qs = self.get_queryset().filter(
+                billing_profile=billing_profile,
+                cart=cart_obj,
+                active=True,
+                status='created'
+            )
+        if qs.count() == 1:
+            obj = qs.first()
+        else:
+            obj = self.model.objects.create(
+                    billing_profile=billing_profile,
+                    cart=cart_obj)
+            created = True
+
 
 
 class Order(models.Model):
@@ -33,6 +123,8 @@ class Order(models.Model):
     def __str__(self):
         return self.order_id
 
+    objects = OrderManager()
+
     def update_total(self):
         cart_total = self.cart.total
         shipping_total = self.shipping_total
@@ -46,9 +138,9 @@ class Order(models.Model):
 def pre_save_create_order_id(sender, instance, *args, **kwargs):
     if not instance.order_id:
         instance.order_id = unique_order_id_generator(instance)
-    # qs = Order.objects.filter(cart=instance.cart).exclude(billing_profile=instance.billing_profile)
-    # if qs.exists():
-    #     qs.update(active=False)
+    qs = Order.objects.filter(cart=instance.cart).exclude(billing_profile=instance.billing_profile)
+    if qs.exists():
+        qs.update(active=False)
     #
     # if instance.shipping_address and not instance.shipping_address_final:
     #     instance.shipping_address_final = instance.shipping_address.get_address()
