4f1cc12154372add7d3eed05bdd23d83947fd479
nemaba <nelsonmb@gmail.com>
2019-01-03 13:20:49 +0100

    csrf ajax js

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.http import JsonResponse, HttpResponse
 
 from .forms import ContactForm
 
@@ -25,10 +26,17 @@ def about_page(request):
 def contact_page(request):
     contact_form = ContactForm(request.POST or None)
     context = {
-        "title": "Contact Page",
-        "content": "Welcome contact page",
-        "form": contact_form
+        "title":"Contact",
+        "content":" Welcome to the contact page.",
+        "form": contact_form,
     }
     if contact_form.is_valid():
         print(contact_form.cleaned_data)
+        if request.is_ajax():
+            return JsonResponse({"message": "Thank you for your submission"})
+
+    if contact_form.errors:
+        errors = contact_form.errors.as_json()
+        if request.is_ajax():
+            return HttpResponse(errors, status=400, content_type='application/json')
     return render(request, "contact/view.html", context)
