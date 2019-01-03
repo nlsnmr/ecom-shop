ee2fe1beb684c26c4497a894b3a72da9ad564047
nemaba <nelsonmb@gmail.com>
2019-01-03 17:21:32 +0100

    add required fields to User model

--- accounts/views.py
+++ accounts/views.py
@@ -1,85 +1,129 @@
-
 from django.contrib.auth import authenticate, login, get_user_model
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
+from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, FormView, DetailView, View, UpdateView
 from django.views.generic.edit import FormMixin
 from django.http import HttpResponse
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.utils.http import is_safe_url
 from django.utils.safestring import mark_safe
 
+from ecommerce.mixins import NextUrlMixin, RequestFormAttachMixin
 from .forms import LoginForm, RegisterForm, GuestForm
 from .models import GuestEmail
+# from .signals import user_logged_in
 
 
-def guest_register_view(request):
-    form = GuestForm(request.POST or None)
-    context = {
-        "form": form
-    }
-    next_ = request.GET.get('next')
-    next_post = request.POST.get('next')
-    redirect_path = next_ or next_post or None
-    if form.is_valid():
-        email = form.cleaned_data.get("email")
-        new_guest_email = GuestEmail.objects.create(email=email)
-        request.session['guest_email_id'] = new_guest_email
+# @login_required # /accounts/login/?next=/some/path/
+# def account_home_view(request):
+#     return render(request, "accounts/home.html", {})
 
-        if is_safe_url(redirect_path, request.get_host()):
-            return redirect(redirect_path)
+
+# LoginRequiredMixin,
+class AccountHomeView(LoginRequiredMixin, DetailView):
+    template_name = 'accounts/home.html'
+
+    def get_object(self):
+        return self.request.user
+
+
+class AccountEmailActivateView(FormMixin, View):
+    pass
+
+    def post(self, request, *args, **kwargs):
+        # create form to receive an email
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
         else:
-            return redirect("/register")
+            return self.form_invalid(form)
 
-    return redirect("/register")
+    def form_valid(self, form):
+        pass
 
-
-def login_page(request):
-    form = LoginForm(request.POST or None)
-    context = {
-        "form": form
-    }
-    next_ = request.GET.get('next')
-    next_post = request.POST.get('next')
-    redirect_path = next_ or next_post or None
-    if form.is_valid():
-        username = form.cleaned_data.get('username')
-        password = form.cleaned_data.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            try:
-                del request.session['guest_email_id']
-            except:
-                pass
-            if is_safe_url(redirect_path, request.get_host()):
-                return redirect(redirect_path)
-            else:
-                # Redirect to a success page.
-                # context['form'] = LoginForm()
-                return redirect("/login")
-
-        else:
-            # Return an 'invalid login' error message.
-            return redirect("/login")
-
-    return render(request, "accounts/login.html", context)
-
-User = get_user_model()
+    def form_invalid(self, form):
+        context = {'form': form, "key": self.key}
+        return render(self.request, 'registration/activation-error.html', context)
 
 
-def register_page(request):
-    form = RegisterForm(request.POST or None)
-    context = {
-        "form": form
-    }
-    if form.is_valid():
-        print(form.cleaned_data)
-        username = form.cleaned_data.get('username')
-        email = form.cleaned_data.get('email')
-        password = form.cleaned_data.get('password')
-        new_user = User.objects.create_user(username, email, password)
+class GuestRegisterView(NextUrlMixin, RequestFormAttachMixin, CreateView):
+    form_class = GuestForm
+    default_next = '/register/'
 
-    return render(request, "accounts/register.html", context)
+    def get_success_url(self):
+        return self.get_next_url()
+
+    def form_invalid(self, form):
+        return redirect(self.default_next)
+
+
+class LoginView(NextUrlMixin, RequestFormAttachMixin, FormView):
+    form_class = LoginForm
+    success_url = '/'
+    template_name = 'accounts/login.html'
+    default_next = '/'
+
+    def form_valid(self, form):
+        next_path = self.get_next_url()
+        return redirect(next_path)
+
+
+class RegisterView(CreateView):
+    form_class = RegisterForm
+    template_name = 'accounts/register.html'
+    success_url = '/login/'
+
+
+class UserDetailUpdateView(LoginRequiredMixin, UpdateView):
+    pass
+
+    def get_object(self):
+        return self.request.user
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(UserDetailUpdateView, self).get_context_data(*args, **kwargs)
+        context['title'] = 'Change Your Account Details'
+        return context
+
+    def get_success_url(self):
+        return reverse("account:home")
+
+# def login_page(request):
+#     form = LoginForm(request.POST or None)
+#     context = {
+#         "form": form
+#     }
+#     next_ = request.GET.get('next')
+#     next_post = request.POST.get('next')
+#     redirect_path = next_ or next_post or None
+#     if form.is_valid():
+#         username  = form.cleaned_data.get("username")
+#         password  = form.cleaned_data.get("password")
+#         user = authenticate(request, username=username, password=password)
+#         if user is not None:
+#             login(request, user)
+#             try:
+#                 del request.session['guest_email_id']
+#             except:
+#                 pass
+#             if is_safe_url(redirect_path, request.get_host()):
+#                 return redirect(redirect_path)
+#             else:
+#                 return redirect("/")
+#         else:
+#             # Return an 'invalid login' error message.
+#             print("Error")
+#     return render(request, "accounts/login.html", context)
+
+# User = get_user_model()
+# def register_page(request):
+#     form = RegisterForm(request.POST or None)
+#     context = {
+#         "form": form
+#     }
+#     if form.is_valid():
+#         form.save()
+#     return render(request, "accounts/register.html", context)
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -19,7 +19,7 @@ from django.contrib.auth.views import LogoutView
 
 from django.conf import settings
 from django.conf.urls.static import static
-from accounts.views import login_page, register_page, guest_register_view
+from accounts.views import LoginView, RegisterView, GuestRegisterView
 from addresses.views import checkout_address_create_view, checkout_address_reuse_view
 from carts.views import cart_detail_api_view
 from .views import home_page, about_page, contact_page
@@ -30,14 +30,14 @@ urlpatterns = [
     url(r'^$', home_page, name='home'),
     url(r'^about/$', about_page, name='about'),
     url(r'^contact/$', contact_page, name='contact'),
-    url(r'^login/$', login_page, name='login'),
+    url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^checkout/address/create/$', checkout_address_create_view, name='checkout_address_create'),
     url(r'^checkout/address/reuse/$', checkout_address_reuse_view, name='checkout_address_reuse'),
-    url(r'^register/guest/$', guest_register_view, name='guest_register'),
+    url(r'^register/guest/$', GuestRegisterView.as_view(), name='guest_register'),
     url(r'^logout/$', LogoutView.as_view(), name='logout'),
     url(r'^api/cart/$', cart_detail_api_view, name='api-cart'),
     url(r'^cart/$', cart_home, name='cart'),
-    url(r'^register/$', register_page, name='register'),
+    url(r'^register/$', RegisterView.as_view(), name='register'),
     url(r'^cart/', include("carts.urls")),
     url(r'^products/', include("products.urls")),
     url(r'^search/', include("search.urls")),
