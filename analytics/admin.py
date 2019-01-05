09649a0e82a7d09e470a3394c955602a54565f16
nemaba <nelsonmb@gmail.com>
2019-01-05 20:51:26 +0100

    sessions and signals

--- analytics/admin.py
+++ analytics/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import ObjectView
+from .models import ObjectViewed, UserSession
 
-admin.site.register(ObjectView)
+
+admin.site.register(ObjectViewed)
+
+admin.site.register(UserSession)
