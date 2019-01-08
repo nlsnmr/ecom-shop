858dca1f03bdc2e4af22d1f80d1d7dc9032b6749
nemaba <nelsonmb@gmail.com>
2019-01-08 21:05:40 +0100

    save card

--- billing/admin.py
+++ billing/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import BillingProfile
+from .models import BillingProfile, Card
 
 admin.site.register(BillingProfile)
+admin.site.register(Card)
--- billing/views.py
+++ billing/views.py
@@ -4,27 +4,30 @@ from django.utils.http import is_safe_url
 
 import stripe
 
+from .models import BillingProfile, Card
+
 stripe.api_key = "sk_test_yT2kdPo6H6R4ux82MsyrkPEG"
 STRIPE_PUB_KEY = 'pk_test_Bz1MpNExcRPDzRW8oJPkdZUc'
 
 
 def payment_method_view(request):
-    #next_url =
-    # if request.user.is_authenticated():
-    #     billing_profile = request.user.billingprofile
-    #     my_customer_id = billing_profile.customer_id
-
-    # billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
-    # if not billing_profile:
-    #     return redirect("/cart")
+    billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
+    if not billing_profile:
+        return redirect("/cart")
     next_url = None
     next_ = request.GET.get('next')
     if is_safe_url(next_, request.get_host()):
         next_url = next_
     return render(request, 'billing/payment-method.html', {"publish_key": STRIPE_PUB_KEY, "next_url": next_url})
 
+
 def payment_method_createview(request):
     if request.method == "POST" and request.is_ajax():
-        print(request.POST)
-        return JsonResponse({"message":"Success! Your card was added."})
-    return HttpResponse("error", status=401)
+        billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
+        if not billing_profile:
+            return HttpResponse({"message": "Cannot find this user"}, status_code=401)
+        token = request.POST.get("token")
+        if token is not None:
+            new_card_obj = Card.objects.add_new(billing_profile, token)
+        return JsonResponse({"message": "Success! Your card was added."})
+    return HttpResponse("error", status_code=401)
