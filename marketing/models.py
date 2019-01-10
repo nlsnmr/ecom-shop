b98f4420ddca70398a2899c7cc89f0626c69206a
nemaba <nelsonmb@gmail.com>
2019-01-10 15:27:10 +0100

    django and mailchimp

--- marketing/models.py
+++ marketing/models.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.db.models.signals import post_save, pre_save
 
-# from .utils import Mailchimp
+from .utils import Mailchimp
 
 
 class MarketingPreference(models.Model):
@@ -24,29 +24,31 @@ def marketing_pref_create_receiver(sender, instance, created, *args, **kwargs):
         print('created')
 
 
-# post_save.connect(marketing_pref_create_receiver, sender=MarketingPreference)
+post_save.connect(marketing_pref_create_receiver, sender=MarketingPreference)
 
 
-# def marketing_pref_update_receiver(sender, instance, *args, **kwargs):
-#     if instance.subscribed != instance.mailchimp_subscribed:
-#         if instance.subscribed:
-#             # subscribing user
-#             status_code, response_data = Mailchimp().subscribe(instance.user.email)
-#         else:
-#             # unsubscribing user
-#             status_code, response_data = Mailchimp().unsubscribe(instance.user.email)
-#
-#         if response_data['status'] == 'subscribed':
-#             instance.subscribed = True
-#             instance.mailchimp_subscribed = True
-#             instance.mailchimp_msg = response_data
-#         else:
-#             instance.subscribed = False
-#             instance.mailchimp_subscribed = False
-#             instance.mailchimp_msg = response_data
-#
-#
-# pre_save.connect(marketing_pref_update_receiver, sender=MarketingPreference)
+def marketing_pref_update_receiver(sender, instance, *args, **kwargs):
+    if instance.subscribed != instance.mailchimp_subscribed:
+        if instance.subscribed:
+
+            # subscribing user
+            status_code, response_data = Mailchimp().subscribe(instance.user.email)
+            print(instance.user.email)
+        else:
+            # unsubscribing user
+            status_code, response_data = Mailchimp().unsubscribe(instance.user.email)
+
+        if response_data['status'] == 'subscribed':
+            instance.subscribed = True
+            instance.mailchimp_subscribed = True
+            instance.mailchimp_msg = response_data
+        else:
+            instance.subscribed = False
+            instance.mailchimp_subscribed = False
+            instance.mailchimp_msg = response_data
+
+
+pre_save.connect(marketing_pref_update_receiver, sender=MarketingPreference)
 
 
 def make_marketing_pref_receiver(sender, instance, created, *args, **kwargs):
