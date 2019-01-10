fffab990f40c771e0a6d5547e6eab49bcb8cb4b6
nemaba <nelsonmb@gmail.com>
2019-01-10 16:44:04 +0100

    webhooks

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,18 +1,3 @@
-"""ecommerce URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth.views import LogoutView
@@ -24,6 +9,7 @@ from addresses.views import checkout_address_create_view, checkout_address_reuse
 from carts.views import cart_detail_api_view
 from .views import home_page, about_page, contact_page
 from billing.views import payment_method_view, payment_method_createview
+from marketing.views import MarketingPreferenceUpdateView, MailchimpWebhookView
 
 from carts.views import cart_home
 
@@ -44,6 +30,8 @@ urlpatterns = [
     url(r'^cart/', include("carts.urls")),
     url(r'^products/', include("products.urls")),
     url(r'^search/', include("search.urls")),
+    url(r'^settings/email/', MarketingPreferenceUpdateView.as_view(), name='marketing-pref'),
+    url(r'^webhooks/mailchimp/', MailchimpWebhookView.as_view(), name='webhooks-mailchimp'),
     url(r'^admin/', admin.site.urls),
 ]
 
